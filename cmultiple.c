51976bf04057f48a95a9bc4c2cab070dc00d1b0f
Jussi Pakkanen <jpakkane@gmail.com>
2015-12-28 00:22:31 +0200

    Added test executable that does multiple calls in one loop.

---
+++ cmultiple.c
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2015 Jussi Pakkanen
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of version 3 of the GNU General Public License as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include"cimpl.h"
+#include"common.h"
+#include<stdlib.h>
+
+int main(int argc, char **argv) {
+    const int should_throw = 0;
+    int i;
+    for(i=0; i<NUM_ITERATIONS/5; i++) {
+        struct Error *err = NULL;
+        do_something(should_throw, &err);
+        if(err) {
+            free_error(err);
+            continue;
+        }
+        do_something(should_throw, &err);
+        if(err) {
+            free_error(err);
+            continue;
+        }
+        do_something(should_throw, &err);
+        if(err) {
+            free_error(err);
+            continue;
+        }
+        do_something(should_throw, &err);
+        if(err) {
+            free_error(err);
+            continue;
+        }
+        do_something(should_throw, &err);
+        if(err) {
+            free_error(err);
+            continue;
+        }
+    }
+    return 0;
+}
